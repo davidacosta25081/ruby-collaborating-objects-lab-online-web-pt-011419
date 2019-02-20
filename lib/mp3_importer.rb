ab5d851c8d0bf803346abdf885ff608926a2d52f
David Acosta <davidacosta25081@yahoo.com>
2019-02-20 05:17:01 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,13 @@ def initialize(path)
    @path = path
 end
 
+def files
+  Dir.entries(path).select {|entry| entry.include?(".mp3")}
+end
+
+  def import
+    self.files.each {|file| Song.new_by_filename(file)}
+end
 
 
-
-  def files
+end
