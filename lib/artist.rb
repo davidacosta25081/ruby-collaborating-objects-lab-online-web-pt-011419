10320b2ecf6cbf19c3f04b3a9a4a4bb4354f3a90
David Acosta <davidacosta25081@yahoo.com>
2019-02-20 04:26:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,11 +20,20 @@ end
 
 def add_song (song)
    @songs << song
+
  end
 
 def save
   @@all << self
 end
+
+def self.find_or_create_by_name(name)
+  if self.name.nil?
+    artist= Artist.new(name)
+  else
+    self.name
+end
+      
 
 
 end
